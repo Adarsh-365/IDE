eb186e6102493015abfab2d2555b90fbaa346222
Adarsh Tayde <adarshtayde9011@gmail.com>
2025-01-16 17:36:28 +0530

    first commit

--- IDE/wsgi.py
+++ IDE/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IDE.settings')
 
+# Initialize the Django WSGI application
 application = get_wsgi_application()
+
+# Wrap the application with WhiteNoise
+application = WhiteNoise(application)
